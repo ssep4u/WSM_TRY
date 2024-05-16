80356cf97dff609d8dd097036becc935b14b7759
SSep <ssep4u@gmail.com>
2024-05-16 09:59:39 +0900

    소스 코드 정리

--- 240512_SchoolMarketDomitory/js/school-food.js
+++ 240512_SchoolMarketDomitory/js/school-food.js
@@ -1,5 +1,6 @@
 let currentDate = new Date();
 
+// 급식 제목: 이전 버튼, 요일(월/일)의 메뉴, 다음 버튼
 const displayDate = () => {
     let days = "일월화수목금토";
     let month = currentDate.getMonth() + 1;
@@ -11,6 +12,7 @@ const displayDate = () => {
     schoolFoodTitleHeader.innerText = titleText;
 };
 
+// 이전, 다음 버튼 눌러 날짜 바꾸자
 const changeDate = (diff) => {
     currentDate.setDate(currentDate.getDate() + diff);
     const dateData = currentDate.toISOString().slice(0, 10).replace(/-/g, "");  //2024-05-15 -> 20240515
@@ -18,17 +20,33 @@ const changeDate = (diff) => {
     displayDate();
 };
 
+// 현재 날짜의 급식 메뉴 가져오자
+const getSchoolFoodMenu = (dateData) => {
+    // https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN17320190722180924242823&infSeq=2
+    const KEY = "5f1eef5fec9e46e098c48b31c3032b58"; //API KEY
+    const TYPE = "json";
+    const ATPT_OFCDC_SC_CODE = "B10";   //시도교육청코드
+    const SD_SCHUL_CODE = "7011569";    //행정표준코드
+    const url = `https://open.neis.go.kr/hub/mealServiceDietInfo?Type=${TYPE}&KEY=${KEY}&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${SD_SCHUL_CODE}&MLSV_YMD=${dateData}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            setSchoolFoodMenu(data);
+        });
+};
+
+// 급식 메뉴 가져온 data 화면에 표시하자
 const setSchoolFoodMenu = (data) => {
     // console.log(data);
     //메뉴 clear
     let breakfastMenuUl = document.getElementsByClassName("breakfast menu")[0];
     let lunchMenuUl = document.getElementsByClassName("lunch menu")[0];
     let dinnerMenuUl = document.getElementsByClassName("menu dinner")[0];
-    breakfastMenuUl.innerHTML = "<li style='width: 100%;'>급식메뉴를 불러오지 못했습니다.</li>";
-    lunchMenuUl.innerHTML = "<li style='width: 100%;'>급식메뉴를 불러오지 못했습니다.</li>";
-    dinnerMenuUl.innerHTML = "<li style='width: 100%;'>급식메뉴를 불러오지 못했습니다.</li>";
+    breakfastMenuUl.innerHTML = "<li style='width: 100%;'>급식 메뉴를 불러오지 못했습니다.</li>";
+    lunchMenuUl.innerHTML = "<li style='width: 100%;'>급식 메뉴를 불러오지 못했습니다.</li>";
+    dinnerMenuUl.innerHTML = "<li style='width: 100%;'>급식 메뉴를 불러오지 못했습니다.</li>";
 
-    if(data.mealServiceDietInfo) {
+    if (data.mealServiceDietInfo) {
         const menuData = data.mealServiceDietInfo[1].row;
         // console.log(menuData);
 
@@ -37,11 +55,11 @@ const setSchoolFoodMenu = (data) => {
             cleanedMenu = cleanedMenu.replace(/\./g, "");       // . 삭제
             let cleanedMenuArray = cleanedMenu.split("<br/>");  //<br/>를 기준으로 나눠서, 각 메뉴 음식 하나씩 구하자
             cleanedMenuArray = cleanedMenuArray.map((element) => element.trim());   // 메뉴음식 좌우 여백 삭제
-            
+
             // array -> <li class="menu-food">차조밥</li>
             let menuFoodLi = "";
             cleanedMenuArray.forEach((menuFood) => {
-                menuFoodLi += `<li class="menu-food">${menuFood}</li>\n`;
+                menuFoodLi += `<li class="menu-food">${menuFood}</li>\n`;   //메뉴 음식 하나씩 <li>에 넣자
             });
 
             if (menuRow.MMEAL_SC_NM === "조식") {
@@ -55,13 +73,5 @@ const setSchoolFoodMenu = (data) => {
     }
 };
 
-const getSchoolFoodMenu = (dateData) => {
-    const url = `https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&KEY=5f1eef5fec9e46e098c48b31c3032b58&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7011569&MLSV_YMD=${dateData}`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            setSchoolFoodMenu(data);
-        });
-};
 displayDate();  /* 제목 표시 */
 changeDate(0);  /* 급식 메뉴 표시 */
