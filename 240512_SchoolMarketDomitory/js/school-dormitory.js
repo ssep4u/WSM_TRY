9bd0f6435d479562c51c894df1aa3d5b8415c817
SSep <ssep4u@gmail.com>
2024-10-11 14:25:28 +0900

    dormitory calendar -> dormitory #calendar

--- 240512_SchoolMarketDomitory/js/school-dormitory.js
+++ 240512_SchoolMarketDomitory/js/school-dormitory.js
@@ -23,8 +23,8 @@ const setCalendarHeader = (currentDate) => {
   const month = currentDate.getMonth();
 
   //제목
-  const calendarHeaderH1 = document.querySelector("#calendar-header > h1");
-  calendarHeaderH1.textContent = `${year}년 ${month + 1}월`;
+  const calendarHeaderH2 = document.querySelector("#calendar-header > h2");
+  calendarHeaderH2.textContent = `${year}년 ${month + 1}월`;
 };
 const setCalendar = (date) => {
   const year = date.getFullYear();
@@ -32,6 +32,8 @@ const setCalendar = (date) => {
   const firstDate = new Date(year, month, 1);
   const firstDay = firstDate.getDay();
   const lastDate = new Date(year, month + 1, 0);
+  const lastDay = lastDate.getDay();
+  const prevMonthLastDate = new Date(year, month, 0);
 
   //달력 요일 만들자
   const calendarContainer = document.querySelector("#calendar-container");
@@ -45,30 +47,57 @@ const setCalendar = (date) => {
   let currentDate = 1;
 
   // 이전 달 뒷 날짜 만들자: 이번 달 1일의 요일 전까지 만들자
-  for (let i = 0; i < firstDay; i++) {
+  //?~이전 달 마지막 날짜  ?: 이전 달 마지막 날짜 - 이번 달 첫날의 요일 + 1
+  console.log(prevMonthLastDate.getDate());
+  for (let date = (prevMonthLastDate.getDate() - firstDay + 1); date <= prevMonthLastDate.getDate(); date++) {
     let prevMonthDate = document.createElement("div");
-    prevMonthDate.textContent =
-      new Date(year, month, 0).getDate() - firstDate.getDay() + i + 1;
     prevMonthDate.className = "item other-month";
+    prevMonthDate.textContent = date;
     calendarContainer.appendChild(prevMonthDate);
   }
 
   //이번 달 날짜 만들자
-  for (let i = currentDate; i < lastDate.getDate(); i++) {
+  for (let date = currentDate; date < lastDate.getDate(); date++) {
     let currentMonthDate = document.createElement("div");
-    currentMonthDate.textContent = i;
     currentMonthDate.className = "item";
+    currentMonthDate.dataset.date = `${year}-${month+1}-${date}`;
+    currentMonthDate.textContent = date;
     calendarContainer.appendChild(currentMonthDate);
   }
 
   //다음 달 앞 날짜 만들자
-  for (let i = lastDate.getDay(); i <= 6; i++) {
+  //1~?  ?: 6-이번 달 마지막 날짜의 요일
+  for (let date = 1; date <= (6 - lastDay + 1); date++) {
     let nextMonthDate = document.createElement("div");
-    nextMonthDate.textContent = i - lastDate.getDay() + 1;
     nextMonthDate.className = "item other-month";
+    nextMonthDate.textContent = date;
     calendarContainer.appendChild(nextMonthDate);
   }
 }
+const setPage = (page) => {
+  page--;
+  const pages = ["dormitory-select-date", "dormitory-select-user", "dormitory-board"];
+  const menuContainerDiv = document.getElementById("menu-container");
 
+  //메뉴 선택 clear
+  for (const menuItemDiv of menuContainerDiv.children) {
+    menuItemDiv.classList.remove("select-menu");
+  }
+  //메뉴 선택 one
+  menuContainerDiv.children[page].classList.add("select-menu");
+
+
+  //display: none;
+  //all page none
+  pages.forEach((pageString) => {
+    let pageDiv = document.getElementById(pageString);
+    pageDiv.style.display = "none";
+  })
+  //page one
+  let pageDiv = document.getElementById(pages[page]);
+  pageDiv.style.display = "block";
+}
+
+setPage(1);
 setCalendarHeader(currentDate);
 setCalendar(currentDate);
